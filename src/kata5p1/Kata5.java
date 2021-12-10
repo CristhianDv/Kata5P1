630760b7582bc32c7de6246797284d349707b2f2
CristhianDv <holbey12@gmail.com>
2021-12-10 22:32:45 +0100

    Segunda versión de Kata5P1

--- src/kata5p1/Kata5.java
+++ src/kata5p1/Kata5.java
@@ -9,6 +9,7 @@ import java.sql.Statement;
 public class Kata5 {
     public static void main(String[] args) {
         Kata5 easy = new Kata5();
+        crearTabla();
         easy.selectAll();
     }
     
@@ -26,21 +27,34 @@ public class Kata5 {
     }
     
     public void selectAll() {
-        String sql = "SELECT * FROM PEOPLE";
+        String sql = "SELECT * FROM EMAIL";
         
         try (   Connection conexion = connect();
                 Statement statement = conexion.createStatement();
                 ResultSet respuesta = statement.executeQuery(sql)){
-            
+            if (!respuesta.next()) System.out.println("La tabla existe pero no tiene contenido");
             while (respuesta.next()){
                 System.out.println(respuesta.getInt("Id") + "\t" + 
-                                   respuesta.getString("Name") + "\t" +
-                                   respuesta.getString("Apellidos") + "\t" + 
-                                   respuesta.getString("Departamento") + "\t");
+                                   respuesta.getString("Mail") + "\t");
             }
         } catch (SQLException e){
             System.out.println(e.getMessage());
         }
+    }
+    
+    public static void crearTabla(){
+        String url = "jdbc:sqlite:KATA5.db";
         
+        String sql = "CREATE TABLE IF NOT EXISTS EMAIL (\n"
+                + " Id integer PRIMARY KEY AUTOINCREMENT,\n"
+                + " Mail text NOT NULL);";
+        
+        try (Connection conexion = DriverManager.getConnection(url);
+                Statement statement = conexion.createStatement()){
+            statement.execute(sql);
+            System.out.println("Tabla creada");
+        } catch (SQLException e1){
+            System.out.println(e1.getMessage());
+        }
     }
 }
